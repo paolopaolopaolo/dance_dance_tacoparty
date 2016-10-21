0cefc59a4eb47aae624b74a41d1ff5a1d82262e3
Dean Mercado <dean@yeti.co>
2016-10-21 12:49:54 -0700

    Project Start: Add general project structure and orchestration

---
+++ config/settings/local.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "O1]9%&meb*]42.yPd7KRMZT:MU<6lgIQU,b/b^qH"
+
+ALLOWED_HOSTS = ['*']
